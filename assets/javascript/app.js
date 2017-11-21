b8d2203f48462e8613bc11535c2e5c6fcdcfba58
Olivia Garrett <oliviajonesgarrett@gmail.com>
2017-11-20 22:33:40 -0500

    html complete, javascript show/hide at start and if/else statements for correct/incorrect answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+//game starts
+$(document).ready(function(){
+	//hide all questions and answers until start button is clicked
+	$("h2").hide();
+	$("h3").hide();
+	$("p").hide();
+	$("input").hide();
+	$("#startButton").click(function() {
+		$(this).hide();
+		$("h2").show();
+		$("h3").show();
+		$("p").show();
+		$("input").show();
+	});
+	//time counter starts - 2:00 minutes
+	function startTime() {
+		document.getElementById('timeRemaining').innerHTML = "Time Remaining: " 
+	}
+	//when time is up, game ends
+	//hide h2, h3, p, input
+	//show #allDone #correctAnswers #incorrectAnswer #unanswered
+	//if value=answers then correctAnswers++
+	if ($('input[value="answer"]').val() == 'answer') {
+		$("#correctAnswers").text("Correct Answers: " + correctAnswers++)
+	} else if ($('input[value="incorrect"]').val() == 'incorrect') {
+		$("#incorrectAnswers").text("Incorrect Answers: " + incorrectAnswers++)
+	} else ($("#unanswered").text("Unanswered: " + unanswered++)) 
+	});
+	//if else value=incorrect then incorrectAnswer++ 
+	//else unanswered++
+
+
+
+
+
