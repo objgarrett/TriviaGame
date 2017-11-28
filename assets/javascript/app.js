0d0207e119067f954eda848339a5271a0c9a3603
Olivia Garrett <oliviajonesgarrett@gmail.com>
2017-11-27 22:41:02 -0500

    updates to javascript - startTime, endTimer, scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,85 @@
 //game starts
 $(document).ready(function(){
-	//hide all questions and answers until start button is clicked
-	$("h2").hide();
-	$("h3").hide();
-	$("p").hide();
-	$("input").hide();
-	$("#startButton").click(function() {
+
+//This holds the setInterval that runs the timer
+var intervalId;
+
+//timer object, starts at 30 seconds
+var timer = {
+	
+	time: 10,
+
+//starts the timer
+startTime: function() {
+	intervalID = setInterval(decrement, 1000);
+	function decrement () {
+		timer.time--;
+		var currentTime = timer.timeConverter(timer.time);
+		$('#timeRemaining').html("Time Remaining: " + currentTime);
+	}	
+},
+
+//takes current time and converts it to minutes and seconds
+timeConverter: function(t) {
+	var minutes = Math.floor(t / 60);
+	var seconds = t - (minutes * 60);
+
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+
+	if (minutes === 0) {
+		minutes = "00";
+	}
+
+	else if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	return minutes + ":" + seconds;
+}
+};
+
+//hide all questions and answers until start button is clicked
+$("h2").hide();
+$("h3").hide();
+$("p").hide();
+$("input").hide();
+$("#startButton").click(timer.startTime); 
+$("#startButton").click(function() {
 		$(this).hide();
+		$("h4").hide();
 		$("h2").show();
 		$("h3").show();
 		$("p").show();
 		$("input").show();
-		startTime();
-	});
+		$('#timeRemaining').html("Time Remaining: 00:10");		
+});
+$("#startButton").click(stopTimer);
 
-	var intervalId;
-	//time counter starts - 2:00 minutes
-	function startTime() {
-		document.getElementById('timeRemaining').innerHTML = "Time Remaining: " 
-	}
-	//when time is up, game ends
-	//hide h2, h3, p, input
-	//show #allDone #correctAnswers #incorrectAnswer #unanswered
-	//if value=answers then correctAnswers++
-	if ($('input[value="answer"]').val() == 'answer') {
-		$("#correctAnswers").text("Correct Answers: " + correctAnswers++)
-	} else if ($('input[value="incorrect"]').val() == 'incorrect') {
-		$("#incorrectAnswers").text("Incorrect Answers: " + incorrectAnswers++)
-	} else ($("#unanswered").text("Unanswered: " + unanswered++)) 
-	});
-	//if else value=incorrect then incorrectAnswer++ 
-	//else unanswered++
+//when time is up, games end -- hide h2, h3, p, input & show h4
+function stopTimer() {
+	setTimeout(function(){
+		$("h2").hide();
+		$("h3").hide();
+		$("p").hide();
+		$("input").hide();
+		$("h4").show();
+	}, 10000);
+	triviaOver();
+};
 
+//shows correct, incorrect, and unanswered scores
+var triviaOver = function() {
+	var correctAnswers = $('input:checked[value=correct]').length; 
+		$("#correctAnswers").text("Correct Answers: " + correctAnswers)
+	//if ($('input:checked[value="correct"]').val() == 'correct') {};	
+	var incorrectAnswers = $('input:checked[value=incorrect]').length;
+		$("#incorrectAnswers").text("Incorrect Answers: " + incorrectAnswers)
+	//var unanswered = 0;
+};
+
+});
 
 
 
